9327feda34c1dc214239cb7990f44854ebaf7a05
premharsha970-png <premharsha970@gmail.com>
2026-08-30 12:45:27 +0530

    Implement built-in commands for shell
    Refactor shell command handling and add built-in commands.

--- myshell.c
+++ myshell.c
@@ -2,25 +2,43 @@
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
+int shell_exit();
+int home();
 char command[100];
 int main(int argc,char **argv)
 {
 printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 printf("welcome to my shell\n");
 char buffer[256];
+struct new{
+    char name[20];
+    int (*func)();
+};  
+struct new builtin[]={
+    {"exit\n", shell_exit},
+    {"home\n", home}
+};
 while(1)
 {
-printf("%s>",getcwd(buffer,256));
+printf("\n%s>",getcwd(buffer,256));
 fgets(command,100,stdin);
-if(strcmp(command,"exit\n")==0)
-{
-break;
-}
-else if (strcmp(command,"home\n")==0)
-{
-chdir("/home");
+for(int i=0;i<3;i++){
+    if(strcmp(command,builtin[i].name)==0){
+        builtin[i].func();}
+
 }
 }
 return 0;
 }
 
+int shell_exit()
+{
+ exit(0);
+}
+
+int home()
+{
+    chdir("/home");
+} 
+
+ 
