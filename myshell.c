43caaa4984127238aa863b778cae26510b94a80a
premharsha970-png <premharsha970@gmail.com>
2026-09-22 18:38:20 +0530

    Enhance shell with color and command handling
    Refactor shell program to improve structure and add color output.

--- myshell.c
+++ myshell.c
@@ -1,44 +1,66 @@
+//including libs
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
-int shell_exit();
-int home();
+#include<sys/wait.h>
 char command[100];
-int main(int argc,char **argv)
+#define color_green "\x1b[32m"
+#define reset_color "\x1b[0m"
+#define bold "\x1b[1m"
+#define MAX_ARGS 10
+int main()
 {
-printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-printf("welcome to my shell\n");
-char buffer[256];
-struct new{
-    char name[20];
-    int (*func)();
-};  
-struct new builtin[]={
-    {"exit\n", shell_exit},
-    {"home\n", home}
-};
-while(1)
-{
-printf("\n%s>",getcwd(buffer,256));
+//a simple visual design for more appealing look
+printf("%s======================================================\n%s",color_green,reset_color);
+printf("%swelcome to my shell%s\n",color_green,reset_color);
+    char buffer[256];
+//the permanent loop that keeps taking input
+    while(1){
+    printf("\n%s> ",getcwd(buffer,256));
 fgets(command,100,stdin);
-for(int i=0;i<3;i++){
-    if(strcmp(command,builtin[i].name)==0){
-        builtin[i].func();}
-
-}
-}
-return 0;
-}
-
-int shell_exit()
+command[strcspn(command,"\n")]=0;
+// tokenizing input 
+ int i=0;
+    char *token[MAX_ARGS];
+     token[0]=strtok(command," \n\r");
+while(token[i]!=NULL && i < MAX_ARGS -1)
+{i++;
+    token[i]=strtok(NULL," \n\r");
+    
+} token[i]=NULL;       
+//builtin commands
+    if(strcmp(command,"exit")==0){
+        exit(0);}
+    else if(strcmp(token[0],"cd")==0)
 {
- exit(0);
+    if(token[1]==NULL)
+    {
+    printf("please enter a directory");
+        continue;
+    }
+
+if(chdir(token[1])!=0){
+    printf("directory not found");
+continue;}
+continue;
+}
+else if (token[0]==NULL)
+{
+continue;
 }
 
-int home()
+//executing system commands      
+pid_t pid = fork();
+if(pid==0)
 {
-    chdir("/home");
-} 
-
- 
+    execvp(token[0],token);
+    printf("command not found %s", token[0]);
+    exit(1);
+}
+else if(pid>0)
+{
+wait(NULL);
+}
+}
+}
